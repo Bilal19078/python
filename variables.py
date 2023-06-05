ecfbd3d427a19d5fdae9a08434d007d45d34d035
Bilal19078 <b_bilal2017@outlook.fr>
2023-06-05 15:05:38 +0200

    Premier commit
    Créateur du repo kebab

---
+++ variables.py
@@ -0,0 +1,57 @@
+# nombre entier, integer
+number1 = 123
+number1 = 42
+print(number1)
+
+#nomnre à virgule flottante, float
+number2 = 3.14
+print(number2)
+
+#chaîne de caractères, string
+text1 = "foo bar baz"
+print(text1)
+
+text2 = "foo bar baz"
+print(text2)
+
+#booléen, boolean
+python_is_cool = True
+print(python_is_cool)
+
+python_is_boring = False
+print(python_is_boring)
+
+#null
+accepted_terms = None
+print(None)
+
+
+# types de données
+print (type (number1))
+print (type(number2))
+print (type(text1))
+print (type(text2))
+print (type(python_is_cool))
+print (type(python_is_boring))
+print (type(user_accepted_terms))
+
+#vérification du type de données
+print(type(number1) is int)
+print(type(number1) is str)
+
+#todo: interroger le type des autres variables...
+
+# transtypage
+print(type(str(number1)))
+print(bool(number1))
+
+
+
+
+
+# les fonctions de transtypage
+#srt() convertit vers un string
+#int() convertit vers un integer
+#float() convertit vers float
+#bool() convertit vert booléan
+
