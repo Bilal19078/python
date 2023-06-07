6b16b69fdbf3ed28a3342ebaa20d895689edb769
Bilal19078 <b_bilal2017@outlook.fr>
2023-06-07 11:17:12 +0200

    exercice-02

---
+++ exercices-02/ifcours.py
@@ -0,0 +1,10 @@
+def even_or_odd(number):
+   # if bool(number % 2):
+   #    return "Odd"
+   # else:
+   #    return "Even"
+
+   if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
